f7e4a078145598c03ac23861816823c9b8d5a571
AmrAmrA <amrelfattah.dev@gmail.com>
2024-02-01 14:25:34 +0100

    Install React hook form

--- P14-HRnet/src/App.jsx
+++ P14-HRnet/src/App.jsx
@@ -2,9 +2,13 @@ import './App.css'
 
 function App() {
   return (
+    <>
     <p className='text-5xl bg-green-500'>
       hello world dfdqsjnkljqdnsk
     </p>
+    <h1 className='text-4xl'>Salut </h1>
+    <p className='text-3xl bg-red-500'>TanStack Table</p>
+    </>
   )
 }
 
