efcdcd9f00da41c0b9a00e02d619a1ba15a2c750
AmrAmrA <amrelfattah.dev@gmail.com>
2024-02-05 14:13:44 +0100

    Modyfying the form

--- P14-HRnet/src/pages/FormPage/FormPage.jsx
+++ P14-HRnet/src/pages/FormPage/FormPage.jsx
@@ -16,18 +16,65 @@ const FormPage = () => {
   };
 
   return (
-    <form className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor='firstName'>Firstname</label>
-      <input htmlFor ='firstName'  type='text' defaultValue={"Enter your Firstname"} {...register("example")} />
-      <label htmlFor='LastName'>Lastname</label>
-      <input  htmlFor ='LastName'  type='text' defaultValue={"Enter your Lastname"} {...register("LastName")} />
-      <label htmlFor='birthDay'>BirthDay</label>
-      <input htmlFor ='birthDay' type='date'></input>
-      <label htmlFor='startDate'>Start Date</label>
-      <input htmlFor ='startDate' type='date'></input>
+    <form className="mt-4 flex flex-col bg-gray-900 rounded-lg p-4 shadow-sm">
+    <h1 className="text-white font-bold text-lg text-center">HRnet</h1>
 
-      <input className='mt-20' type="submit" />
-    </form>
+   
+        <label className="text-white mt-4"  htmlFor="FirstName">First Name</label>
+        <input placeholder="Your Firstname" className="w-full bg-gray-800 rounded-md border-gray-700 text-white px-2 py-1" type="text" required />
+ 
+
+        <label className="text-white mt-4" htmlFor="FirstName">Last Name</label>
+        <input placeholder="Your Lastname" className="w-full bg-gray-800 rounded-md border-gray-700 text-white px-2 py-1"  type="text" required/>
+
+        <label className="text-white mt-4" htmlFor="FirstName">Date of Birth</label>
+        <input placeholder="" className="w-full bg-gray-800 rounded-md border-gray-700 text-white px-2 py-1"  type="date" required/>
+        
+        <label className="text-white mt-4" htmlFor="FirstName">Date of start</label>
+        <input placeholder="" className="w-full bg-gray-800 rounded-md border-gray-700 text-white px-2 py-1" type="date" required />
+
+      
+    <fieldset className ="address mt-20">
+    <legend className='text-white'> Address </legend>
+    <div className="mt-4">
+        <label className="text-white" htmlFor="address">Street</label>
+        <input placeholder="Your address" className="w-full bg-gray-800 rounded-md border-gray-700 text-white px-2 py-1" id="address" required></input>
+    </div>
+
+    <div className="mt-4 flex flex-row space-x-2">
+        <div className="flex-1">
+            <label className="text-white" htmlFor="city">City</label>
+            <input placeholder="Your city" className="w-full bg-gray-800 rounded-md border-gray-700 text-white px-2 py-1" id="city" type="text" required/>
+        </div>
+
+        <div className="flex-1">
+            <label className="text-white" htmlFor="state">State</label>
+            <input placeholder="Your state" className="w-full bg-gray-800 rounded-md border-gray-700 text-white px-2 py-1" id="state" type="text" required/>
+        </div>
+    </div>
+
+   
+        <div className="mt-7 flex flex-row">
+            <label className="text-white text-center py-1" htmlFor="zip">ZIP</label>
+            <input placeholder="Your ZIP code" className="w-3/6 bg-gray-800 rounded-md border-gray-700 text-white px-2 py-1 mx-3" id="zip" type="number" required/>
+            <label className="text-white text-center py-1" htmlFor="zip">ZIP</label>
+            <input placeholder="Your ZIP code" className="w-3/6 bg-gray-800 rounded-md border-gray-700 text-white px-2 py-1 mx-3" id="zip" type="number" required/>
+        </div>
+ 
+    </fieldset>
+
+    <label htmlFor="department" className='text-white mt-5 mb-1'>Departement</label>
+    <select name="department" id="department" className='w-2/6 '>
+      <option>Sales</option>
+      <option>Marketing</option>
+      <option>Engineering</option>
+      <option>Human Resources</option>
+      <option>Legal</option>
+    </select>
+    <div className="mt-4 flex justify-end">
+        <button className="bg-white text-black rounded-md px-4 py-1 hover:bg-blue-500 hover:text-white transition-all duration-200" type="submit" required>Save</button>
+    </div>
+</form>
   )
 };
 
